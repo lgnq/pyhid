0422233d40ef1a9255bfc89f30286e1e78b9d6b6
lgnq <dzzxzz@gmail.com>
2018-08-05 12:22:59 +0800

    add class Thread

--- pyhid.py
+++ pyhid.py
@@ -1,8 +1,27 @@
 import sys
 
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from mainwindow import Ui_MainWindow
+
+class Thread(QtCore.QThread):
+    msg_ready = QtCore.pyqtSignal(list)
+
+    def __init__(self, func):
+        super(QtCore.QThread, self).__init__()
+        self.func = func
+
+    def run(self):
+        while True:
+            msg = []
+            items = self.func()
+
+            if items:
+                for i in items:
+                    msg.append(i)
+
+                self.msg_ready.emit(msg)
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
