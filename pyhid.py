a43b06051e6046c6395498119d41a0d65d952cf4
lgnq <dzzxzz@gmail.com>
2018-08-05 16:41:18 +0800

    no Thread needed
    display the received data from CP2110

--- pyhid.py
+++ pyhid.py
@@ -1,30 +1,10 @@
 import sys
-import queue
 import pywinusb.hid as hid
 
-from PyQt5 import QtCore
+# from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 from mainwindow import Ui_MainWindow
-
-
-class Thread(QtCore.QThread):
-    msg_ready = QtCore.pyqtSignal(list)
-
-    def __init__(self, func):
-        super(QtCore.QThread, self).__init__()
-        self.func = func
-
-    def run(self):
-        while True:
-            msg = []
-            items = self.func()
-
-            if items:
-                for i in items:
-                    msg.append(i)
-
-                self.msg_ready.emit(msg)
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -38,7 +18,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.hid_device = None  # 设备
 
-        self.queue = queue.Queue()  # 创建队列
+        self.receive_buff = " "
 
         self.actionScan.triggered.connect(self.scan)
         self.actionExit.triggered.connect(self.close)
@@ -49,29 +29,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.baudrate_combobox.currentIndexChanged.connect(self.baudrate_change)
         self.open_pushbutton.clicked.connect(self.device_openclose)
 
-        self.thread = Thread(self.queue_monitor)
-        self.thread.msg_ready.connect(self.rx_textbrowser_update)
-        # self.thread.start()
-
-    def queue_monitor(self):
-        if self.queue.qsize():
-            try:
-                msgs = self.queue.get()
-                return msgs
-
-            except queue.Empty:
-                pass
-
-    string = ""
-    def rx_textbrowser_update(self, item):
-        if item[0] == 1:
-            if item[1] == 10:
-                self.textBrowser.append(self.string)
-                # self.bar.setValue(self.bar.maximum())
-                self.string = ""
-            elif item[1] != 13:
-                self.string = self.string + chr(item[1])
-
     def about(self):
         QMessageBox.question(self, 'About', "CP2110 USB-to-UART\r\nVersion: 1.0\r\nAuthor: lgnq", QMessageBox.Ok, QMessageBox.Ok)
 
@@ -80,7 +37,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # self.widget.device_scan()
 
     def report_recv_handler(self, data):
-        self.queue.put(data)
+        if data[0] == 1:
+            if data[1] == 10:
+                self.rx_textbrowser.append(self.receive_buff)
+                self.receive_buff = ""
+            elif data[1] != 13:
+                self.receive_buff = self.receive_buff + chr(data[1])
 
     def baudrate_change(self):
         if self.device_combobox.count() == 0:
